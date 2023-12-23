ff11fe94c11672aab45c11a0cb3f5bb4a665b30d
Jian Lik Ng <jlik.lik@gmail.com>
2023-12-23 11:42:53 -0800

    Tried day8 part2, but I think brute force is NOT the way to go

--- src/day8.rs
+++ src/day8.rs
@@ -53,7 +53,6 @@ impl DesertMap {
       for dir in &dir_vec {
           (steps, key) = Self::recur_find_path(dmap, dir, key, steps);
           if key == "ZZZ" {
-            dbg!("Found zzz");
             dbg!((&key, &dir, &steps));
             break;
           }
@@ -76,6 +75,67 @@ impl DesertMap {
     };
     (steps + 1, next_key)
   }
+
+  pub fn find_path_2(dmap: &Self, directions: String) -> i32 {
+    let dir_vec: Vec<char> = directions.chars().collect();
+
+    let mut steps = 0;
+    let mut keys = Self::get_starting_keys(dmap);
+    for k in &keys {
+      println!("steps: {}, new key: {}", steps, k);
+    }
+    let mut all_zs = false;
+    while !all_zs {
+      for dir in &dir_vec {
+        let mut new_keys = Vec::<String>::new();
+        for key in &keys {
+          let new_key = Self::find_next_key(dmap, dir, key);
+          new_keys.push(new_key);
+        }
+        steps = steps + 1;
+        for k in &new_keys {
+          println!("steps: {}, new key: {}", steps, k);
+        }
+        all_zs = new_keys.iter().all(|x| {
+          let last = x.chars().last().unwrap();
+          if last == 'Z' { true } else { false }
+        });
+        if all_zs {
+          // Finished
+          break;
+        }
+        keys = new_keys;
+      }
+    }
+
+    steps
+    
+  }
+
+  pub fn find_next_key(dmap: &Self, direction: &char, key: &String) -> String {
+    let Some(node) = dmap.nodes.get(key) else {
+      panic!("Node not found in map!");
+    };
+    let next_key = match direction {
+      'L' => node.get_left(),
+      'R' => node.get_right(),
+      _ => panic!("unknown!")
+    };
+    next_key
+  }
+
+  fn get_starting_keys(dmap: &Self) -> Vec<String> {
+    let mut starts = Vec::<String>::new();
+    for (k, v) in &dmap.nodes {
+      let last = k.chars().last().unwrap();
+      if last == 'A' {
+        starts.push(k.clone());
+      }
+    }
+    starts
+  }
+
+
 }
 
 pub struct Day8 {
@@ -93,9 +153,8 @@ impl Day8 {
     }
   }
 
-  fn part1<P>(path_to_input: P) -> i32
-  where P: AsRef<Path>, {
-
+  fn create_dmap<P>(path_to_input: P) -> (DesertMap, String)
+  where P: AsRef<Path> {
     let file = File::open(path_to_input).unwrap();
     let mut buffer = io::BufReader::new(file);
 
@@ -122,6 +181,13 @@ impl Day8 {
         DesertMap::insert_node(&mut dmap, key, node);
       }
     }
+    (dmap, directions)
+  }
+
+  fn part1<P>(path_to_input: P) -> i32
+  where P: AsRef<Path>, {
+
+    let (dmap, directions) = Self::create_dmap(path_to_input);
     DesertMap::list_nodes(&dmap);
     let ans = DesertMap::find_path(&dmap, directions);
 
@@ -130,7 +196,10 @@ impl Day8 {
 
   fn part2<P>(path_to_input: P) -> i32
   where P: AsRef<Path>, {
-    0
+    let (dmap, directions) = Self::create_dmap(path_to_input);
+    DesertMap::list_nodes(&dmap);
+    let ans = DesertMap::find_path_2(&dmap, directions);
+    ans
   }
 
   fn read_line(buffer: &mut io::BufReader<File>) -> Option<String> {
@@ -145,12 +214,6 @@ impl Day8 {
     }
   }
 
-  // fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-  // where P: AsRef<Path>, {
-  //   let file = File::open(filename)?;
-  //   Ok(io::BufReader::new(file).lines())
-  // }
-
 }
 
 #[cfg(test)]
@@ -158,16 +221,23 @@ mod tests {
     use super::*;
 
     #[test]
-    fn works_on_sample_input() {
+    fn part1_works_on_sample_input() {
       let part1 = Day8::part1("./inputs/day8_test.input");
       dbg!(part1);
       assert!(part1 == 6);
     }
 
     #[test]
-    fn works_on_sample_input_2() {
+    fn part1_works_on_sample_input_2() {
       let part1 = Day8::part1("./inputs/day8_test2.input");
       dbg!(part1);
       assert!(part1 == 2);
+    }
+
+    #[test]
+    fn part2_works_on_sample_input() {
+      let part2 = Day8::part2("./inputs/day8_p2_test.input");
+      dbg!(part2);
+      assert!(part2 == 6);
     }
 }
