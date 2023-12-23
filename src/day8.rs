a755862140054120a5407f370471863afa489ce8
Jian Lik Ng <jlik.lik@gmail.com>
2023-12-23 13:42:53 -0800

    Finished day 8 part 2

--- src/day8.rs
+++ src/day8.rs
@@ -44,72 +44,69 @@ impl DesertMap {
     }
   }
 
-  pub fn find_path(dmap: &Self, directions: String) -> i32 {
+  fn find_path(dmap: &Self, start_key: &String, directions: &String, end_cond_fn: &dyn Fn(&String) -> bool) -> i32 {
     let dir_vec: Vec<char> = directions.chars().collect();
-
-    let mut key = "AAA".to_string();
+    let mut key = start_key.clone();
     let mut steps = 0;
-    while key != "ZZZ" {
+    while !(end_cond_fn(&key)) {
       for dir in &dir_vec {
-          (steps, key) = Self::recur_find_path(dmap, dir, key, steps);
-          if key == "ZZZ" {
-            dbg!((&key, &dir, &steps));
-            break;
-          }
-          dbg!((&key, &dir, &steps));
-      }
-    }
-    steps
-  }
-
-  pub fn recur_find_path(dmap: &Self, direction: &char, key: String, steps: i32) -> 
-    (i32, String)
-  {
-    let Some(node) = dmap.nodes.get(&key) else {
-      panic!("Node not found in map!");
-    };
-    let next_key = match direction {
-      'L' => node.get_left(),
-      'R' => node.get_right(),
-      _ => panic!("unknown!")
-    };
-    (steps + 1, next_key)
-  }
-
-  pub fn find_path_2(dmap: &Self, directions: String) -> i32 {
-    let dir_vec: Vec<char> = directions.chars().collect();
-
-    let mut steps = 0;
-    let mut keys = Self::get_starting_keys(dmap);
-    for k in &keys {
-      println!("steps: {}, new key: {}", steps, k);
-    }
-    let mut all_zs = false;
-    while !all_zs {
-      for dir in &dir_vec {
-        let mut new_keys = Vec::<String>::new();
-        for key in &keys {
-          let new_key = Self::find_next_key(dmap, dir, key);
-          new_keys.push(new_key);
-        }
+        key = Self::find_next_key(dmap, dir, &key);
         steps = steps + 1;
-        for k in &new_keys {
-          println!("steps: {}, new key: {}", steps, k);
-        }
-        all_zs = new_keys.iter().all(|x| {
-          let last = x.chars().last().unwrap();
-          if last == 'Z' { true } else { false }
-        });
-        if all_zs {
-          // Finished
+        if end_cond_fn(&key) {
+          // dbg!((&key, &dir, &steps));
           break;
         }
-        keys = new_keys;
       }
     }
-
     steps
-    
+  }
+
+  // taken from https://gist.github.com/victor-iyi/8a84185c1d52419b0d4915a648d5e3e1
+  pub fn gcd(mut n: u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0);
+    while m != 0 {
+      if m < n {
+        std::mem::swap(&mut m, &mut n);
+      }
+      m %= n;
+    }
+    n
+  }
+
+  // Brute forcing this is NOT the way to go
+  // Try using Lowest Common Denominator to solve this problem instead
+  // Algo:
+  // - Find num steps for each one
+  // - Find lowest common denominator for all steps
+  pub fn find_path_2(dmap: &Self, directions: &String) -> u64 {
+    let start_keys = Self::get_starting_keys(dmap);
+    fn ends_with_z(s: &String) -> bool {
+      let last = s.chars().last().unwrap();
+      last == 'Z'
+    }
+    let mut steps_vector = Vec::new();
+    for start_key in &start_keys {
+      let steps = Self::find_path(dmap, start_key, directions, &ends_with_z);
+      dbg!((start_key, steps));
+      steps_vector.push(steps as u64);
+    }
+
+    let Some(mut a)= steps_vector.pop() else {
+      panic!("empty steps");
+    }; 
+    let mut gcd = a;
+    let mut lcm: u128 = a as u128;
+    for _i in 0..steps_vector.len() {
+      let Some(b)= steps_vector.pop() else {
+        panic!("empty steps");
+      }; 
+      gcd = Self::gcd(a, b);
+      lcm = (lcm as u128) * (b as u128) / gcd as u128;
+      a = gcd;
+      println!("gcd: {}", gcd);
+      println!("lcm: {}", lcm);
+    }
+    lcm as u64
   }
 
   pub fn find_next_key(dmap: &Self, direction: &char, key: &String) -> String {
@@ -140,7 +137,7 @@ impl DesertMap {
 
 pub struct Day8 {
   pub part1: i32,
-  pub part2: i32
+  pub part2: u64
 }
 
 impl Day8 {
@@ -188,17 +185,20 @@ impl Day8 {
   where P: AsRef<Path>, {
 
     let (dmap, directions) = Self::create_dmap(path_to_input);
-    DesertMap::list_nodes(&dmap);
-    let ans = DesertMap::find_path(&dmap, directions);
+    // DesertMap::list_nodes(&dmap);
+    fn zzz(s: &String) -> bool {
+      s.clone() == "ZZZ".to_string()
+    }
+    let ans = DesertMap::find_path(&dmap, &"AAA".to_string(), &directions, &zzz);
 
     ans
   }
 
-  fn part2<P>(path_to_input: P) -> i32
+  fn part2<P>(path_to_input: P) -> u64
   where P: AsRef<Path>, {
     let (dmap, directions) = Self::create_dmap(path_to_input);
-    DesertMap::list_nodes(&dmap);
-    let ans = DesertMap::find_path_2(&dmap, directions);
+    // DesertMap::list_nodes(&dmap);
+    let ans = DesertMap::find_path_2(&dmap, &directions);
     ans
   }
 
